35b786f565f9e9ed9140a3a68a28363c2db57630
Domantas Jadenkus <djadenkus@gmail.com>
2016-06-03 23:36:47 +0300

    server game base

---
+++ HackerGamesVilnius/game.js
@@ -0,0 +1,71 @@
+﻿function Game(io) {
+    this.io = io;
+    this.players = [];
+    this.nextState = 0;
+}
+
+Game.prototype.moveTo = function (id, x, y) {
+    var player = this.getPlayer(id);
+    player.tx = x;
+    player.ty = y;
+}
+
+Game.prototype.joined = function (socket, id) {
+    var player = {
+        id: id,
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+        tx: 0,
+        ty: 0,
+    }
+    player.tx = player.x;
+    player.ty = player.y;
+    this.players.push(player);
+    
+    socket.emit('players', this.players);
+    this.io.emit('joined', player);
+}
+
+Game.prototype.left = function (id) {
+    for (var i = 0; i < this.players.length; i++)
+        if (this.players[i].id == id) {
+            this.players.splice(i, 1);
+            return;
+        }
+    
+    this.io.emit('left', id);
+}
+
+Game.prototype.getPlayer = function (id) {
+    for (var i = this.players.length; i--; )
+        if (this.players[i].id == id)
+            return this.players[i];
+
+    throw new Error("player with id='" + id + "' was not found");
+}
+
+Game.prototype.step = function (dt) {
+    
+    this.nextState -= dt;
+    if (this.nextState < 0) {
+        this.nextState += 0.5;
+        this.io.emit('players', this.players);
+    }
+
+    for (var i = this.players.length; i--; ) {
+        var p = this.players[i];
+        var dx = p.tx - p.x;
+        var dy = p.ty - p.y;
+        var dist = Math.sqrt(dx * dx + dy * dy);
+        if (dist > 50 * dt) {
+            p.x += dx / dist * 50 * dt;
+            p.y += dy / dist * 50 * dt;
+        } else {
+            p.x = p.tx;
+            p.y = p.ty;
+        }
+    }
+
+}
+
+module.exports = Game;
--- HackerGamesVilnius/server.js
+++ HackerGamesVilnius/server.js
@@ -3,17 +3,35 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var gameFn = require('./game.js');
+var game = new gameFn(io);
+
 // serves all the static files
 app.use(express.static('static'));
 
 var listeningPort = /*process.env.PORT || parseInt(process.argv[2]) ||*/ 8000;
 http.listen(listeningPort, function () { console.log('listening on *:' + listeningPort); });
 
+var lastTime = Date.now();
+setInterval(function () {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+    console.log('stepping, passed: ' + dt);
+    game.step(dt);
+    lastTime = now;
+}, 1000.0 / 10.0);
+
 io.on('connect', function (socket) {
     
-    console.log("connected: " + socket.handshake.address);
+    console.log("connected: " + socket.id + " from " + socket.handshake.address);
+    game.playerJoined(socket.id);
 
     socket.on('disconnect', function () {
-        console.log("disconnected: " + socket.handshake.address);
+        console.log("disconnected: " + socket.id);
+        game.playerLeft(socket.id);
+    });
+
+    socket.on('pointerdown', function (data) {
+        game.moveTo(socket.id, data.x, data.y);
     });
 });
