ebd4b7bc9a1b0a8c3e073585f7c4301afbe58fa4
wrehas <wrehas@gmail.com>
2016-06-03 22:52:49 +0300

    cause u are lazy af

--- HackerGamesVilnius/server.js
+++ HackerGamesVilnius/server.js
@@ -6,14 +6,14 @@ var io = require('socket.io')(http);
 // serves all the static files
 app.use(express.static('static'));
 
-var listeningPort = process.env.PORT || parseInt(process.argv[2]) || 8000;
+var listeningPort = /*process.env.PORT || parseInt(process.argv[2]) ||*/ 8000;
 http.listen(listeningPort, function () { console.log('listening on *:' + listeningPort); });
 
 io.on('connect', function (socket) {
     
-    console.log("connected: " + JSON.stringify(socket));
+    console.log("connected: " + socket.handshake.address);
 
     socket.on('disconnect', function () {
-        console.log("disconnected: " + JSON.stringify(socket));
+        console.log("disconnected: " + socket.handshake.address);
     });
 });
